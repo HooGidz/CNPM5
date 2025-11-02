15a3e2f2072b42ee823245e71259141fbfee3651
TaiAnh88 <tranthienbao050@gmail.com>
2025-11-02 20:42:17 +0700

    thêm quản lý sinh viên

--- CNPM5/Models/Cnpm5Context.cs
+++ CNPM5/Models/Cnpm5Context.cs
@@ -19,18 +19,21 @@ public partial class Cnpm5Context : DbContext
 
     public virtual DbSet<TblRole> TblRoles { get; set; }
 
-    public virtual DbSet<TblSinhVien>TblSinhViens { get; set; }
+    public virtual DbSet<TblStudent> TblStudents { get; set; }
 
-    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-SFNTDCJC\\ANHTAI;Initial Catalog=CNPM5;Integrated Security=True;TrustServerCertificate=True;");
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<TblAccount>(entity =>
         {
-            entity.HasKey(e => e.AccountId).HasName("PK__tblAccou__349DA5A6AB334ADF");
+            entity.HasKey(e => e.AccountId).HasName("PK__tblAccou__349DA5A6FE950CE2");
 
             entity.ToTable("tblAccount");
 
-            entity.HasIndex(e => e.Username, "UQ__tblAccou__536C85E4CCE54131").IsUnique();
+            entity.HasIndex(e => e.Username, "UQ__tblAccou__536C85E494A42A67").IsUnique();
 
             entity.Property(e => e.CreatedDate)
                 .HasDefaultValueSql("(getdate())")
@@ -49,7 +52,7 @@ public partial class Cnpm5Context : DbContext
 
         modelBuilder.Entity<TblRole>(entity =>
         {
-            entity.HasKey(e => e.RoleId).HasName("PK__tblRole__8AFACE1A85DA442B");
+            entity.HasKey(e => e.RoleId).HasName("PK__tblRole__8AFACE1A38872D91");
 
             entity.ToTable("tblRole");
 
@@ -58,30 +61,42 @@ public partial class Cnpm5Context : DbContext
             entity.Property(e => e.RoleName).HasMaxLength(50);
         });
 
-        modelBuilder.Entity<TblSinhVien>(entity =>
+        modelBuilder.Entity<TblStudent>(entity =>
         {
-            entity.ToTable("tblSinhVien");
-            entity.HasKey(e => e.MaSV);
-            entity.Property(e => e.HoTen).IsRequired().HasMaxLength(100);
-            entity.Property(e => e.GioiTinh).IsRequired().HasMaxLength(10);
-            entity.Property(e => e.NgaySinh)
-                  .HasColumnType("date");
+            entity.HasKey(e => e.StudentId).HasName("PK__tblStude__32C52B994B416683");
 
-            entity.Property(e => e.Khoa)
-                  .HasMaxLength(100);
+            entity.ToTable("tblStudents");
 
-            entity.Property(e => e.Lop)
-                  .HasMaxLength(50);
+            entity.HasIndex(e => e.StudentCode, "UQ__tblStude__1FC886040597C1A2").IsUnique();
 
-            entity.Property(e => e.SDT)
-                  .HasMaxLength(15);
+            entity.HasIndex(e => e.CitizenId, "UQ__tblStude__6E49FBEDA2132640").IsUnique();
 
-            entity.Property(e => e.Email)
-                  .HasMaxLength(100);
+            entity.Property(e => e.AvatarUrl).HasMaxLength(250);
+            entity.Property(e => e.CitizenId)
+                .HasMaxLength(20)
+                .HasColumnName("CitizenID");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.EmergencyContactName).HasMaxLength(100);
+            entity.Property(e => e.EmergencyContactPhone).HasMaxLength(20);
+            entity.Property(e => e.Faculty).HasMaxLength(100);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.Gender).HasMaxLength(10);
+            entity.Property(e => e.Major).HasMaxLength(100);
+            entity.Property(e => e.PermanentAddress).HasMaxLength(250);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.StudentCode).HasMaxLength(20);
+            entity.Property(e => e.StudentStatus).HasMaxLength(50);
+            entity.Property(e => e.TemporaryAddress).HasMaxLength(250);
+            entity.Property(e => e.UpdatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
 
-            entity.Property(e => e.DiaChi)
-                  .HasMaxLength(200);
-            entity.Property(e => e.TrangThai).HasDefaultValue("Đang ở");
+            entity.HasOne(d => d.Account).WithMany(p => p.TblStudents)
+                .HasForeignKey(d => d.AccountId)
+                .HasConstraintName("FK__tblStuden__Accou__534D60F1");
         });
 
         OnModelCreatingPartial(modelBuilder);
--- CNPM5/Models/TblAccount.cs
+++ CNPM5/Models/TblAccount.cs
@@ -24,4 +24,6 @@ public partial class TblAccount
     public DateTime CreatedDate { get; set; }
 
     public string? Status { get; set; }
+
+    public virtual ICollection<TblStudent> TblStudents { get; set; } = new List<TblStudent>();
 }
